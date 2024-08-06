59bf8140c0283a3da3bfdde7d63db65e324b1573
lhduyanh02 <lhduyanh2002@gmail.com>
2024-08-06 11:23:26 +0700

    init project

---
+++ src/test/java/com/lhduyanh/garagemanagement/GarageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhduyanh.garagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
